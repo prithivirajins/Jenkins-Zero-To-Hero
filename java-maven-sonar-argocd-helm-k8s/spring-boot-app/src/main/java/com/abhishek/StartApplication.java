87e122a253a87990fdbc1ea9c460e2758c37107c
prithivirajins <125144111+prithivirajins@users.noreply.github.com>
2026-03-29 08:51:23 -0700

    Update StartApplication.java
    changes in mave

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Mave");
+        model.addAttribute("title", "I have successfuly built a sprint boot application using Mav");
         model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
         return "index";
     }
